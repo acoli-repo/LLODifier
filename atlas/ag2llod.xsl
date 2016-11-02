d109e3160408d4a9894731e88944d8e65be36c30
chiarcos <christian.chiarcos@web.de>
2016-11-02 01:09:36 +0100

    added ag:nextAnchor property

--- atlas/ag2llod.xsl
+++ atlas/ag2llod.xsl
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0" xmlns:atlas="http://www.ldc.upenn.edu/atlas/ag/">
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0" xmlns:ag="http://www.ldc.upenn.edu/atlas/ag/">
 
     <!-- LLODifier for Annotation Graphs (ATLAS/AIF level 0) -->
     <xsl:output method="text" indent="no"/>
@@ -10,7 +10,7 @@
         <!-- write TTL header -->
         <xsl:text>PREFIX dc: &lt;http://purl.org/dc/elements/1.1/&gt;&#10;</xsl:text>
         <xsl:text>PREFIX xlink: &lt;http://www.w3.org/1999/xlink/&gt;&#10;</xsl:text>
-        <xsl:text>PREFIX atlas: &lt;http://www.ldc.upenn.edu/atlas/ag/&gt;&#10;</xsl:text>   <!-- this was given in the examples, doesn't resolve, an actual [but invalid] DTD is under http://xml.coverpages.org/atlas-ag-DTD.txt -->
+        <xsl:text>PREFIX ag: &lt;http://www.ldc.upenn.edu/atlas/ag/&gt;&#10;</xsl:text>   <!-- this was given in the examples, doesn't resolve, an actual [but invalid] DTD is under http://xml.coverpages.org/atlas-ag-DTD.txt -->
         <xsl:text>PREFIX : &lt;</xsl:text>
         <xsl:value-of select="$baseURI"/>
         <xsl:text>#&gt;&#10;</xsl:text>
@@ -21,32 +21,32 @@
     </xsl:template>
     
     <!-- annotation header and metadata -->
-    <xsl:template match="atlas:AGSet">
+    <xsl:template match="ag:AGSet">
         <xsl:text>&lt;</xsl:text>
         <xsl:value-of select="$baseURI"/>
-        <xsl:text>&gt; atlas:</xsl:text>
+        <xsl:text>&gt; ag:</xsl:text>
         <xsl:value-of select="translate(name(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')"/>
         <xsl:text> :</xsl:text>
         <xsl:value-of select="@id"/>
         <xsl:text>.&#10;</xsl:text>
         <xsl:text>:</xsl:text>
         <xsl:value-of select="@id"/>
-        <xsl:text> a atlas:</xsl:text>
+        <xsl:text> a ag:</xsl:text>
         <xsl:value-of select="name()"/>
         <xsl:for-each select="@*">
             <xsl:if test="not(contains(name(),':')) and string-length(normalize-space(string(.)))&gt;0 and name()!='id'">
-                <xsl:text>;&#10;  atlas:</xsl:text>
+                <xsl:text>;&#10;  ag:</xsl:text>
                 <xsl:value-of select="name()"/>
                 <xsl:text> "</xsl:text>
                 <xsl:value-of select="."/>
                 <xsl:text>"</xsl:text>
             </xsl:if>
         </xsl:for-each>
-        <xsl:for-each select="atlas:Metadata/*">
+        <xsl:for-each select="ag:Metadata/*">
             <xsl:if test="string-length(normalize-space(string(.)))&gt;0">
                 <xsl:text>;&#10;  </xsl:text>
                 <xsl:if test="not(contains(name(),':'))">
-                    <xsl:text>atlas:</xsl:text>
+                    <xsl:text>ag:</xsl:text>
                 </xsl:if>
                 <xsl:value-of select="name()"/>
                 <xsl:text> "</xsl:text>
@@ -60,35 +60,35 @@
     </xsl:template>
     
     <!-- see AGset -->
-    <xsl:template match="atlas:Metadata"/>
+    <xsl:template match="ag:Metadata"/>
     
     <!-- NB: Timeline does *not* provide timeline information, but identifies the audio files associated with a particular timeline -->
-    <xsl:template match="atlas:Timeline|atlas:Signal|atlas:AG|atlas:Anchor|atlas:Annotation">
+    <xsl:template match="ag:Timeline|ag:Signal|ag:AG|ag:Annotation">
         <xsl:text>:</xsl:text>
         <xsl:value-of select="../@id"/>
-        <xsl:text> atlas:</xsl:text>
+        <xsl:text> ag:</xsl:text>
         <xsl:value-of select="translate(name(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')"/>
         <xsl:text> :</xsl:text>
         <xsl:value-of select="@id"/>
         <xsl:text>.&#10;</xsl:text>
         <xsl:text>:</xsl:text>
         <xsl:value-of select="@id"/>
-        <xsl:text> a atlas:</xsl:text>
+        <xsl:text> a ag:</xsl:text>
         <xsl:value-of select="name()"/>
         
         <!-- object properties -->
         <xsl:for-each select="@timeline|@start|@end">
-            <xsl:text>;&#10;  atlas:</xsl:text>
+            <xsl:text>;&#10;  ag:</xsl:text>
             <xsl:value-of select="name()"/>
             <xsl:text> :</xsl:text>
             <xsl:value-of select="."/>
         </xsl:for-each>
         
         <!-- datatype properties -->
-        <xsl:for-each select="@*[name()!='id' and name()!='timeline' and string-length(normalize-space(.))&gt;0]">
+        <xsl:for-each select="@*[name()!='id' and name()!='timeline' and name()!='start' and name()!='end' and string-length(normalize-space(.))&gt;0]">
             <xsl:text>;&#10;  </xsl:text>
             <xsl:if test="not(contains(name(),':'))">
-                <xsl:text>atlas:</xsl:text>
+                <xsl:text>ag:</xsl:text>
             </xsl:if>
             <xsl:value-of select="name()"/>
             <xsl:text> "</xsl:text>
@@ -98,13 +98,61 @@
         <xsl:text>.&#10;&#10;</xsl:text>
         <xsl:apply-templates/>
     </xsl:template>
-    
-    <!-- note that we have to disentangle datatype properties ("normal") and object properties (e.g., for alignment, coref) *heuristically*:
-         we check whether the feature value represents an id in the local file -->
-    <xsl:template match="atlas:Feature">
+
+    <!--
+        we introduce a novel nextAnchor property, holding between an anchor and the next with the same @signals attribute
+        we assume that Anchors with same @signals attribute are ordered, otherwise, it makes no sense to require @unit, but to imply @offset in the DTD 
+    -->
+    <xsl:template match="ag:Anchor">
         <xsl:text>:</xsl:text>
         <xsl:value-of select="../@id"/>
-        <xsl:text> atlas:</xsl:text>
+        <xsl:text> ag:</xsl:text>
+        <xsl:value-of select="translate(name(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')"/>
+        <xsl:text> :</xsl:text>
+        <xsl:value-of select="@id"/>
+        <xsl:text>.&#10;</xsl:text>
+        <xsl:text>:</xsl:text>
+        <xsl:value-of select="@id"/>
+        <xsl:text> a ag:</xsl:text>
+        <xsl:value-of select="name()"/>
+        
+        <!-- object properties -->
+        <xsl:for-each select="@timeline|@start|@end">
+            <xsl:text>;&#10;  ag:</xsl:text>
+            <xsl:value-of select="name()"/>
+            <xsl:text> :</xsl:text>
+            <xsl:value-of select="."/>
+        </xsl:for-each>
+        
+        <!-- datatype properties and object properties with multiple values -->
+        <xsl:for-each select="@*[name()!='id' and name()!='timeline' and name()!='start' and name()!='end' and string-length(normalize-space(.))&gt;0]">
+            <xsl:text>;&#10;  </xsl:text>
+            <xsl:if test="not(contains(name(),':'))">
+                <xsl:text>ag:</xsl:text>
+            </xsl:if>
+            <xsl:value-of select="name()"/>
+            <xsl:text> "</xsl:text>
+            <xsl:value-of select="."/>
+            <xsl:text>"</xsl:text>
+        </xsl:for-each>
+        
+        <!-- next anchor -->
+        <xsl:variable name="signals" select="@signals"/>
+        <xsl:for-each select="./following-sibling::anchor[@signals=$signals][1]">
+            <xsl:text>;&#10;  ag:nextAnchor :</xsl:text>
+            <xsl:value-of select="@id"/>
+        </xsl:for-each>
+        
+        <xsl:text>.&#10;&#10;</xsl:text>
+        <xsl:apply-templates/>
+    </xsl:template>
+
+    <!-- note that we have to disentangle datatype properties ("normal") and object properties (e.g., for alignment, coref) *heuristically*:
+         we check whether the feature value represents an id in the local file -->
+    <xsl:template match="ag:Feature">
+        <xsl:text>:</xsl:text>
+        <xsl:value-of select="../@id"/>
+        <xsl:text> ag:</xsl:text>
         <xsl:choose>
             <xsl:when test="@name!=''">
                 <xsl:value-of select="@name"/>
---
+++ vis/AG2dot.java
@@ -0,0 +1,190 @@
+import java.io.*;
+import java.util.*;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.query.*;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+public class AG2dot {
+
+	public static void main(String[] argv) throws Exception {
+		if(argv.length==0 || Arrays.asList(argv).toString().contains("-?")) 
+			System.err.println("AG2dot [URL|File] [-?] [-keepIDs] [-skip prop1[..n]]\n"+
+				"\tFile     RDF file\n"+
+				"\tURL      URL of an RDF file\n"+
+				"\t-?       this text\n"+
+				"\t-keepIDs keep attributes with local name \"id\" (by default suppressed)\n"+
+				"\t-skip    skip the following properties from the output\n"+
+				"reads from file and writes Dot file *of the complete document* to stdout\n");
+		
+		boolean keepIDs = Arrays.asList(argv).toString().toLowerCase().contains("-keepids"); 
+		String url = argv[0];
+		Model model = ModelFactory.createDefaultModel();
+		model.read(url);
+		
+		// skip removable properties
+		int arg = 0;
+		HashSet<Statement> removables = new HashSet<Statement>();
+		while(arg<argv.length && !argv[arg].toLowerCase().equals("-skip")) arg++;
+		while(arg<argv.length) {
+			String query = "SELECT ?a ?b WHERE { ?a <"+argv[arg]+"> ?b.}";
+			ResultSet results = QueryExecutionFactory.create(QueryFactory.create(query),model).execSelect();
+			while(results.hasNext()) {
+				QuerySolution result = results.next();
+				removables.add(
+						ResourceFactory.createStatement(
+								(Resource) result.get("a"),
+								ResourceFactory.createProperty(argv[arg]), 
+								result.get("b")));
+			}
+			arg++;
+		}
+		for(Statement removable : removables)
+			model.remove(removable);
+		
+		String query = 
+			"PREFIX atlas: <http://www.ldc.upenn.edu/atlas/ag/>\n"+
+			"SELECT ?initialSentence "
+			+ "WHERE { ?initialSentence a nif:Sentence. "
+					+ "FILTER(NOT EXISTS { [] nif:nextSentence ?initialSentence })"
+			+ "} ORDER BY ?initialSentence LIMIT 1";
+		  try (QueryExecution qexec = QueryExecutionFactory.create(QueryFactory.create(query),model)) {
+			ResultSet initialSentences = qexec.execSelect() ;
+			while( initialSentences.hasNext() ) {
+			  Resource initialSentence = initialSentences.nextSolution().getResource("initialSentence");
+
+			  System.out.println("digraph G {\n"
+					+ "subgraph nif_Words {\n"
+					+ "node [shape=plaintext];\n"
+					+ "rank=same;");
+			  
+			  // fetch all Words of the current sentence and their annotations
+			  query =
+				"PREFIX nif: <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#>\n"+
+				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
+				"SELECT ?word ?label ?anno ?nextWord\n"
+				+ "WHERE { <"+initialSentence+"> nif:firstWord/nif:nextWord* ?word.\n"
+						+ "OPTIONAL { ?word rdfs:label ?label. }\n"
+						+ "OPTIONAL { ?word nif:nextWord ?nextWord. }\n"
+						+ "{ SELECT ?word ( group_concat ( distinct ?val;separator=\"<BR/>\" ) as ?anno )\n"
+						  + "WHERE { OPTIONAL { ?word rdfs:label ?string }\n"
+						          + "?word ?rel ?obj.\n"
+						          + "FILTER isLiteral(?obj)\n";
+			  if(!keepIDs)
+				  query = query   + "FILTER (!regex(str(?rel),'.*[#/]id$'))\n";
+			  query=query         + "FILTER (?rel!=rdfs:label) \n"
+						          //+ "BIND(concat(replace(str(?rel),'.*[#/]',''),'=',?obj) AS ?val)\n"
+						          + "BIND(?obj AS ?val)\n"
+						  + "} GROUP BY ?word ORDER BY ?word ?rel ?val\n"
+					    + "}\n"
+					    //+ "BIND(concat(?label,'<BR/>',?anno) AS ?node_label)\n"
+						//+ "BIND(concat(?label,'<BR/><FONT POINT-SIZE=\\'8\\'>',?anno,'</FONT>') AS ?node_label)\n"
+				+ "}";
+			  ResultSet words = QueryExecutionFactory.create(QueryFactory.create(query), model).execSelect();
+				
+				while(words.hasNext()) {
+					QuerySolution word = words.next();
+					System.out.println(escapeURI(word.get("word"))+
+							" [label=<"+escapeLabel(word.get("label"))+
+							"<BR/><FONT POINT-SIZE=\"10\">"+escapeAnno(word.get("anno"))+"</FONT>>];");
+					if(word.get("nextWord")!=null) System.out.println(escapeURI(word.get("word"))+" -> "+escapeURI(word.get("nextWord"))+" [color=none];");
+				}
+				
+				System.out.println("}");
+		
+				System.out.println("subgraph nif_Phrases {\n");
+				// System.out.println("splines=ortho");	// nicer edges, but don't go together with edge labels
+				
+				// flatten reification
+				RSIterator reifications = model.listReifiedStatements();
+				while(reifications.hasNext()) {
+					ReifiedStatement r = reifications.next();
+					model.add(r.getStatement());
+					//System.out.println(r.getStatement());
+				}
+				
+				// write phrase nodes
+				query=	"PREFIX nif: <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#>\n"+
+						"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
+						"SELECT DISTINCT ?phrase ?label ?anno\n"
+						+ "WHERE { <"+initialSentence+"> nif:firstWord/nif:nextWord*/nif:subString+ ?phrase.\n"
+								+ "OPTIONAL { ?phrase rdfs:label ?label . }\n"
+								+ "OPTIONAL { "
+								+ "SELECT ?phrase (group_concat(distinct ?val;separator='<BR/>') AS ?anno)\n"
+								+ "WHERE { ?phrase ?prop ?val.\n"
+								+ "         FILTER isLiteral(?val)\n";
+				  if(!keepIDs)
+					  query = query   
+					  			+ "			FILTER (!regex(str(?prop),'.*[#/]id$'))\n";
+				  query=query   + "} GROUP BY ?phrase ORDER BY ?phrase ?rel\n"
+								+ "}\n"
+						+ "}\n";
+						ResultSet phrases = QueryExecutionFactory.create(QueryFactory.create(query), model).execSelect();
+						
+						while(phrases.hasNext()) {
+							QuerySolution phrase = phrases.next();
+							System.out.print(escapeURI(phrase.get("phrase"))+" [label=<");
+							if(phrase.get("label")==null && phrase.get("anno")==null)
+								System.out.print("&nbsp;");
+							if(phrase.get("label")!=null)
+								System.out.print(escapeLabel(phrase.get("label")));
+							if(phrase.get("label")!=null && phrase.get("anno")!=null)
+								System.out.print("<BR/>");
+							if(phrase.get("anno")!=null)
+								//System.out.print("<FONT POINT-SIZE=\"10\">"+phrase.get("anno")+"</FONT>");
+								System.out.print(escapeAnno(phrase.get("anno")));
+							System.out.println(">];");
+						}
+				
+				// write edges
+				query=	"PREFIX nif: <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#>\n"+
+						"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
+						"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"+
+						"SELECT DISTINCT ?phrase ?child ?anno\n"
+						+ "WHERE { <"+initialSentence+"> nif:firstWord/nif:nextWord*/nif:subString+ ?phrase.\n"
+								+ "?child nif:subString ?phrase.\n"
+								+ "OPTIONAL { "
+								+ "SELECT ?child ?phrase (group_concat(distinct ?val;separator='<BR/>') AS ?anno)\n"
+								+ "WHERE { ?child ^rdf:subject ?rel. ?rel rdf:predicate nif:subString; rdf:object ?parent.\n"
+								+ "			?rel ?prop ?val.\n"
+								+ "         FILTER isLiteral(?val)\n"
+								+ "} GROUP BY ?child ?phrase ORDER BY ?child ?phrase ?rel\n"
+								+ "}\n"
+						+ "}\n";
+						phrases = QueryExecutionFactory.create(QueryFactory.create(query), model).execSelect();
+						
+						while(phrases.hasNext()) {
+							QuerySolution phrase = phrases.next();
+							System.out.print(escapeURI(phrase.get("phrase"))+" -> "+escapeURI(phrase.get("child"))+" [arrowhead=none");
+							if(phrase.get("anno")!=null)
+								System.out.print(",xlabel=<"+escapeAnno(phrase.get("anno"))+">");	// xlabel is a little bit more compact than label
+							System.out.println("];");
+						}
+
+				System.out.println("}");
+				  
+				System.out.println("}");
+			}
+		  }
+	}
+
+	private static String escapeAnno(RDFNode rdfNode) {
+		return StringEscapeUtils.escapeHtml4(rdfNode.toString())
+				.replaceAll("&lt;","<")
+				.replaceAll("&gt;",">")
+				.replaceAll("&quot;","\"")
+				.replaceAll("&amp;","&");
+	}
+
+	private static String escapeLabel(RDFNode rdfNode) {
+		return StringEscapeUtils.escapeHtml4(rdfNode.toString());
+		//.replaceAll("ö","oe").replaceAll("ß", "ss");
+	}
+
+	private static String escapeURI(RDFNode rdfNode) {
+		return rdfNode.toString().replaceAll("[/:#\\.]", "");
+	}
+}
+
+		
+			
+		
